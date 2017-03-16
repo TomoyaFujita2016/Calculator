8c5c38a2ac3d80f9d0241bfe71b0e8b6381c1aba
Tomoya Fujita <fujita.t.2016c@gmail.com>
2017-03-16 19:01:47 +0900

    Removed a few bugs

--- app/src/main/java/example/calculater/MainActivity.java
+++ app/src/main/java/example/calculater/MainActivity.java
@@ -24,30 +24,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final int KEY_7 = 7;
     private final int KEY_8 = 8;
     private final int KEY_9 = 9;
-    private final int KEY_CLEAR = 10;
+    private final int KEY_PLUS = 10;
     private final int KEY_MINUS = 11;
-    private final int KEY_PLUS = 12;
-    private final int KEY_EQUAL = 13;
-    private final int KEY_Dot = 14;
-    private final int KEY_Times = 15;
-    private final int KEY_Divide = 16;
-
+    private final int KEY_Times = 12;
+    private final int KEY_Divide = 13;
+    private final int KEY_EQUAL = 14;
+    private final int KEY_Dot = 15;
+    private final int KEY_CLEAR = 16;
+    private final int PMTD = 17;
 
 
     int buttonId[] = {R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
             R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
-            R.id.buttonClear, R.id.buttonMinus, R.id.buttonPlus, R.id.buttonEqual, R.id.buttonDot,
-            R.id.buttonTimes, R.id.buttonDivide
+            R.id.buttonPlus, R.id.buttonMinus, R.id.buttonTimes, R.id.buttonDivide,
+            R.id.buttonEqual, R.id.buttonDot, R.id.buttonClear
     };
 
     Button button[];
-    TextView displayTv;
+    TextView displayTv, totalTv;
     ScrollView scrollView;
     float total = 0;
     float value;
     String nowValue;
-
+    int lastValue = 99, secondLastValue = 10;
+    boolean byFstLetter = true;
+    boolean byZeroWatcher = false;  /*To protect double Zero when byFstLetter+1 is true*/
+    boolean byDotWatcher = false;   /*To protect double Dot when ~~~*/
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -55,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button = new Button[buttonId.length];
         displayTv = (TextView) findViewById(R.id.display);
         scrollView = (ScrollView) findViewById(R.id.scv);
+        totalTv = (TextView) findViewById(R.id.displayTotal);
 
         for (i = 0; i < buttonId.length; i++) {
             button[i] = (Button) findViewById(buttonId[i]);
@@ -67,48 +72,129 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
         scrollView.scrollTo(0, displayTv.getBottom());
-//
 
 
         for (i = 0; i < buttonId.length; i++) {
             if (view == button[i]) {
-                if (i < 10) {
-                    String nowValue = displayTv.getText().toString();
+                String nowValue = displayTv.getText().toString();
+
+                if (0 < i && i < 10) {
+
+                    /*
+                    if (2 < nowValue.length()) {
+                        secondLastValue = Integer.parseInt("" + nowValue.charAt(nowValue.length() - 2));
+                    }
+                    */
+
                     nowValue += i;
+                    lastValue = i;
                     displayTv.setText(nowValue);
+                    byFstLetter = false;
+
+                    /*
+                    if (byFstLetter && lastValue == 0) {
+                        nowValue = nowValue.substring(0, nowValue.length() - 1);
+                    }
+                    */
+
                 }
 
-                switch (i){
+                if (i == 0) {
+                    if ((byFstLetter && lastValue != 0) || (!byZeroWatcher && !byFstLetter)) {
+                        nowValue += i;
+                        lastValue = i;
+                        displayTv.setText(nowValue);
+                        if (byFstLetter) byZeroWatcher = true;
+                        byFstLetter = false;
+                    }
+                }
+
+                switch (i) {
                     case KEY_CLEAR:
-                        total = 0;
+                        calc();
                         displayTv.setText("");
+                        byFstLetter = true;
+                        lastValue = KEY_CLEAR;
                         break;
-
                     case KEY_PLUS:
-                        nowValue = displayTv.getText().toString();
-                        nowValue += "+";
-                        displayTv.setText(nowValue);
-                        break;
-                    case KEY_MINUS:
-                        nowValue = displayTv.getText().toString();
-                        nowValue += "-";
-                        displayTv.setText(nowValue);
 
+
+                        if (lastValue == KEY_MINUS || lastValue == KEY_Times || lastValue == KEY_Divide) {
+                            nowValue = nowValue.substring(0, nowValue.length() - 1);
+                            nowValue += "+";
+                            lastValue = KEY_PLUS;
+                        } else if (lastValue != KEY_PLUS) {
+                            nowValue += "+";
+                            lastValue = KEY_PLUS;
+                        }
+                        displayTv.setText(nowValue);
+                        byFstLetter = true;
+                        break;
+
+                    case KEY_MINUS:
+
+                        if (lastValue == KEY_PLUS || lastValue == KEY_Times || lastValue == KEY_Divide) {
+                            nowValue = nowValue.substring(0, nowValue.length() - 1);
+                            nowValue += "-";
+                            lastValue = KEY_MINUS;
+                        } else if (lastValue != KEY_MINUS) {
+                            nowValue += "-";
+                            lastValue = KEY_MINUS;
+                        }
+                        displayTv.setText(nowValue);
+                        byFstLetter = true;
+                        break;
+
+                    case KEY_Times:
+
+                        if (lastValue == KEY_MINUS || lastValue == KEY_PLUS || lastValue == KEY_Divide) {
+                            nowValue = nowValue.substring(0, nowValue.length() - 1);
+                            nowValue += "×";
+                            lastValue = KEY_Times;
+                        } else if (lastValue != KEY_Times) {
+                            nowValue += "×";
+                            lastValue = KEY_Times;
+                        }
+                        displayTv.setText(nowValue);
+                        byFstLetter = true;
+                        break;
+
+                    case KEY_Divide:
+                        if (lastValue == KEY_MINUS || lastValue == KEY_Times || lastValue == KEY_PLUS) {
+                            nowValue = nowValue.substring(0, nowValue.length() - 1);
+                            nowValue += "÷";
+                            lastValue = KEY_Divide;
+                        } else if (lastValue != KEY_Divide) {
+                            nowValue += "÷";
+                            lastValue = KEY_Divide;
+                        }
+                        displayTv.setText(nowValue);
+                        byFstLetter = true;
+                        break;
+                    case KEY_Dot:
+                        if(!byFstLetter && !byDotWatcher){
+                            nowValue += ".";
+                            displayTv.setText(nowValue);
+                            byDotWatcher = true;
+                            lastValue = KEY_Dot;
+                        }
                         break;
                     case KEY_EQUAL:
-                        nowValue = String.valueOf(total);
-                        displayTv.setText(nowValue);
-                }
 
+                        break;
+                }
 
 
             }
 
         }
-
+        totalTv.setText(lastValue + " " + byFstLetter + " " + byZeroWatcher + " " + byDotWatcher);
+        if (lastValue != 0) byZeroWatcher = false;
+        if(byFstLetter) byDotWatcher = false;
     }
 
-    private float calc(){
+    private float calc() {
+
         return (float) 0.0;
     }
 
